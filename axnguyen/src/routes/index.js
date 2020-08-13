9b7f1299e571e686857b2dbeb2a485013f088a42
Anna Nguyen <axn3@cornell.edu>
2020-08-13 05:10:50 -0400

    Set up file structure

---
+++ axnguyen/src/routes/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+import {HomeScreen} from "../screens/homeScreen";
+import {AboutScreen} from "../screens/aboutScreen";
+import NavBar from "../components/navbar";
+import Footer from "../components/footer";
+
+function Routes(props) {
+  return (
+    <div>
+      <div>
+        <Router>
+          <nav><NavBar /></nav>
+          <Switch>
+            <Route exact path={"/"} component={HomeScreen} />
+            <Route exact path={"/about"} component={AboutScreen} />
+          </Switch>
+        </Router>
+      </div>
+      <footer><Footer /></footer>
+    </div>
+  );
+}
+
+export default Routes;
