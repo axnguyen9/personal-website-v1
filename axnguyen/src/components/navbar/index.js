d5dc9bfe07618a823897d2d0ce060e102c4f2c73
Anna Nguyen <axn3@cornell.edu>
2020-08-14 00:22:27 -0400

    Added fonts and set up basic navbar

--- axnguyen/src/components/navbar/index.js
+++ axnguyen/src/components/navbar/index.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+
+const useStyles = makeStyles((theme) => ({
+  navBar: {
+    height: '20vh',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    border: '1px solid black',
+  },
+  logo: {
+    height: '100%',
+    border: '1px solid black',
+  },
+}));
 
 export default function NavBar() {
+  const classes = useStyles();
   return(
-    <div>
-      <h1>Navbar</h1>
+    <div className={classes.navBar}>
+      <Link to="/" className={classes.logo}>
+        <img src={logo} className={classes.logo}/>
+      </Link>
+      <Typography>ABOUT</Typography>
     </div>
   );
 }
